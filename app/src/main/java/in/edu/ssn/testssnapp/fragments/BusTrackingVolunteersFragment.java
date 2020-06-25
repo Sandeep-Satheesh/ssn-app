98276a84a7a5265c8f21c199889ddb61647856f2
Pakkeer Mydeen <pakkeermydeenn18072@it.ssn.edu.in>
2020-06-25 22:36:20 +0530

    boolean added

--- app/src/main/java/in/edu/ssn/testssnapp/fragments/BusTrackingVolunteersFragment.java
+++ app/src/main/java/in/edu/ssn/testssnapp/fragments/BusTrackingVolunteersFragment.java
@@ -55,6 +55,7 @@ public class BusTrackingVolunteersFragment extends Fragment {
     String routeNo, userId;
     DatabaseReference busLocRef;
     boolean darkMode;
+    boolean volunteer;
     volatile boolean isSharingLoc = false;
 
     @Override
